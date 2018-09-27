f4482041a1e90778387870193361d250c65051b8
deividasp <deividas.popelskis@gmail.com>
2018-09-27 15:48:01 +0300

    Add additional form validation clause

--- src/main/java/com/github/deividasp/mailtiply/mail/form/Form.java
+++ src/main/java/com/github/deividasp/mailtiply/mail/form/Form.java
@@ -1,6 +1,7 @@
 package com.github.deividasp.mailtiply.mail.form;
 
 import com.github.deividasp.mailtiply.mail.form.element.field.InputField;
+import com.github.deividasp.mailtiply.mail.form.element.field.InputFieldType;
 
 import java.util.Arrays;
 import java.util.List;
@@ -14,6 +15,11 @@ public abstract class Form {
     private void validate() {
         if (getFields().size() < 1)
             throw new RuntimeException("Form does not contain any fields");
+
+        for (InputFieldType type : InputFieldType.values()) {
+            if (getFields().stream().filter(f -> f.type() == type).count() > 1)
+                throw new RuntimeException("Form contains multiple fields of the same type: " + type.toString());
+        }
     }
 
     public List<InputField> getFields() {
